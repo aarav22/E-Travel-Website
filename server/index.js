92f40f9b927c082110608a3cb45b58c4012f718d
Aarav Varshney <aaravvarshney22@gmail.com>
2020-10-30 18:03:30 +0530

    index-server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,28 @@
 // Modules in the backend: 
 var express = require('express'); // To ease working with node
+const bodyParser = require('body-parser'); // For retreiving values from req.body
+const cors = require('cors'); // For cross origin requests 
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 var app = express(); 
+
+// Connection with MongoDB Atlas
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri,
+    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }
+);
+
+const connection = mongoose.connection; 
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+app.use(bodyParser.urlencoded({ extended: true })); //  middleware for parsing bodies from URL
+app.use(bodyParser.json()); // middleware for parsing json objects
+app.use(cors()); 
+
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
