2de13f0f826f05b1f27a4e42128c49a6fde4b3c7
Argha Chakrabarty <ac380012@gmail.com>
2020-10-30 21:43:01 +0530

    css-init

--- src/components/homepage.component/homepage.component.js
+++ src/components/homepage.component/homepage.component.js
@@ -1,18 +1,14 @@
 import React from 'react'; 
 import SearchBar from './searchbar/searchbar.component'
-import DatePicker from './datepicker/datepicker.component'
 import './homepage.component.css'
 
 export default function Homepage(props) { 
     return (
-        <div>
-            <p> Hello, you're at the Homepage </p>
+        <div className="home-main">
+
             <div className="search-box">
                 <SearchBar></SearchBar>
-                <SearchBar></SearchBar>
             </div>
-           
-            {/* <DatePicker></DatePicker> */}
         </div>
     )
 }
