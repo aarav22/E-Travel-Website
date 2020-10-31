5b5366c7d35603bb3411387986dc9a57ec72b44c
Argha Chakrabarty <ac380012@gmail.com>
2020-10-31 21:43:49 +0530

    nav-init

--- src/components/homepage.component/homepage.component.js
+++ src/components/homepage.component/homepage.component.js
@@ -1,13 +1,12 @@
 import React from 'react'; 
-import SearchBar from './searchbar/searchbar.component'
+// import SearchBar from './searchbar/searchbar.component'
 import './homepage.component.css'
 
 export default function Homepage(props) { 
     return (
         <div className="home-main">
-
             <div className="search-box">
-                <SearchBar></SearchBar>
+                {/* <SearchBar></SearchBar> */}
             </div>
         </div>
     )
