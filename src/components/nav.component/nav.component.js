723bbbdc0a6dbb79b909d1a8e7b59cdcb87c08b5
khushi678 <khushi.mehta678@gmail.com>
2020-11-01 07:52:36 +0530

    homepage

--- src/components/nav.component/nav.component.js
+++ src/components/nav.component/nav.component.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./nav.component.css";
+import SearchBar from '../homepage.component/searchbar/searchbar.component'
 
 const Navbar = () => {
     return (
         <div className="navbar">
-            <nav className="nav-nav">
+            <nav className="nav-nav-login">
                 <NavItemLogin />
+            </nav>
+            <nav className="nav-nav-search">
+                <NavSearchBar />
             </nav>
         </div>
     )
@@ -22,5 +26,13 @@ const NavItemLogin = () => {
         </li>
     )
 }
+
+const NavSearchBar = () => {
+    return (
+    <div className="search-box">
+            <SearchBar></SearchBar>
+    </div>
+        )
+    }
 
 export default Navbar;
