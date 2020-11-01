8e87482258d94357f9b5c3c5c2e1bc0a79e9db05
Aarav Varshney <aaravvarshney22@gmail.com>
2020-11-01 22:07:36 +0530

    profile-pic

--- src/components/nav.component/nav.component.js
+++ src/components/nav.component/nav.component.js
@@ -25,19 +25,6 @@ const Navbar = () => {
     )
 }
 
-// const NavItemLogin = () => {
-//     return (
-//         <li className="nav-item nav-login">
-//             <button className="nav-login-button">
-//                 <i className='fa fa-user'></i>
-//                 <LoginHook/>
-//                 <span className="nav-login-text">Login</span>
-//             </button>
-            
-//         </li>
-//     )
-// }
-
 const NavSearchBar = () => {
     return (
     <div className="search-box">
