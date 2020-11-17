aa0e83453e6adb14d8629bbf58899316e4d59a46
Argha Chakrabarty <ac380012@gmail.com>
2020-11-17 15:24:26 +0530

    hotel-page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Error from "./components/error.component/error.component";
 import Navbar from "./components/nav.component/nav.component";
 import Address from "./components/profile.component/add.profile.component";
 import FlightPage from "./components/flight-page.component/flight-page.component";
+import HotelPage from "./components/hotel-page.component/hotel-page.component";
 
 
 function App() {
@@ -20,6 +21,8 @@ function App() {
         <Route path="/add" component={Address} />
 
         <Route path="/flights" component={FlightPage} />
+
+        <Route path="/hotels" component={HotelPage} />
 
         <Route path="*" component={Error} />
       </Switch>
---
+++ src/components/hotel-page.component/hotel-page.component.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./hotel-page.component.css";
+
+
+const HotelPage = () => {
+  return (
+    <div className="hotel-page-container">
+      Hotel Page
+    </div>
+  )
+}
+
+export default HotelPage
