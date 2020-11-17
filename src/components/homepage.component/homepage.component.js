1086ff57b4d9027e78af3ab29d4a25136668c88e
Argha Chakrabarty <ac380012@gmail.com>
2020-11-17 10:24:21 +0530

    home0.7

--- src/components/homepage.component/homepage.component.js
+++ src/components/homepage.component/homepage.component.js
@@ -1,34 +1,22 @@
-import React, {useState} from 'react'; 
-import Search from "./search"
+import React from 'react';
+import Search from "./search";
 import './homepage.component.css';
-// import {
-    // MuiPickersUtilsProvider,
-    // DateTimePicker
-// } from '@material-ui/pickers';
 
 
-export default function Homepage(props) {  
-    const [selectedDate, handleDateChange] = useState(new Date());
+export default function Homepage() {
 
-    
-
-
-    return ( 
-        // <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <div className="home-main">
-                <div className="home-left-pane">
-                    <div className="home-text-wrapper">
-                        <span className="home-text">
-                            book a ride on one of our futuristic flying cars and let it transfer you to the world of 2081.
-                        </span>
-                    </div>
-                </div>
-                <div className="home-right-pane">
-                    <Search placeholder={"Enter Destination..."}/>
-                    <Search placeholder={"Enter Arrival..."}/>
-                    {/* <DateTimePicker value={selectedDate} onChange={handleDateChange} /> */}
-                </div>
-            </div>
-        // </MuiPickersUtilsProvider>
-    )
+  return (
+    <div className="home-main">
+      <div className="home-left-pane">
+        <div className="home-text-wrapper">
+          <span className="home-text">
+            {/*  book a ride on one of our futuristic flying cars and let it transfer you to the world of 2081. */}
+            some text describing our website or simply put, dummy text
+          </span>
+        </div>
+      </div>
+      <div className="home-right-pane">
+      </div>
+    </div>
+  )
 }
