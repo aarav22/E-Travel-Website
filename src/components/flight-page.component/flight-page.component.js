b9fd1ea8fe454a7dc9bdea2fd5661f67d6bf3786
Argha Chakrabarty <ac380012@gmail.com>
2020-11-18 12:54:41 +0530

    hotel-page0.25

--- src/components/flight-page.component/flight-page.component.js
+++ src/components/flight-page.component/flight-page.component.js
@@ -1,11 +1,11 @@
 import Axios from "axios";
-import React, { useState, useEffect } from "react";
-import { useSelector } from 'react-redux';
+import React, {useState, useEffect} from "react";
+import {useSelector} from 'react-redux';
 import amadeusResponse from '../../testing_data/amadeusResponse.json'
 import Pagination from '../pagination.component/pagination.component';
 import "./flight-page.component.css";
 
-const Amadeus = require('amadeus');
+// const Amadeus = require('amadeus');
 
 
 const data = [
@@ -75,7 +75,7 @@ const FlightPage = () => {
   //   clientId: `${process.env.REACT_APP_AMADEUS_API}`,
   //   clientSecret: `${process.env.REACT_APP_AMADEUS_SECRET}`
   // });
-  
+
   useEffect(() => {
     const fetchFlights = async () => {
       setLoading(true);
@@ -118,7 +118,8 @@ const FlightPage = () => {
     <div className="flight-page-container">
       <div className="flight-page-topbar">
         <input placeholder="destination"></input>
-        <input placeholder="arrival"></input>
+          ↔
+          <input placeholder="arrival"></input>
         <div className="info">
           <h5>414 Results</h5>
           <button>Sort By</button>
@@ -128,44 +129,41 @@ const FlightPage = () => {
       <div className="flight-page-main">
         <div className="flight-page-filters"></div>
 
-        <div className="flight-page-flights">
-          {
-            currentFlights.map((flight) => {
-              return (
-                <div className="flight-card" key={flight.id}>
-                  <div className="flight-card-details">
-                    <div className="airline-details">
-                      <svg className="airline-img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 470 470"><path d="m468.9 280c-1.4-2.3-3.8-3.6-6.4-3.6h-133.5c-4.1 0-7.5 3.4-7.5 7.5s3.4 7.5 7.5 7.5h121l-2.5 4.7c-3 5.5-14.6 11.7-25.8 11.7h-120.3c3.3-9 5.2-18.8 5.2-29.1 0-14.4-3.6-28-9.9-39.6h68.4c4.1 0 7.5-3.4 7.5-7.5s-3.4-7.5-7.5-7.5h-78.9c-7.9-8.9-17.7-15.7-28.7-19.7v-75.6c0-12-7.4-22.8-19.4-28.2-1.9-0.9-4.2-0.9-6.1 0-12 5.4-19.4 16.2-19.4 28.2v75.6c-11 4-20.8 10.8-28.7 19.7h-78.9c-4.1 0-7.5 3.4-7.5 7.5s3.4 7.5 7.5 7.5h68.4c-6.3 11.6-9.9 25.2-9.9 39.6 0 10.3 1.8 20.1 5.2 29.1h-120.3c-11.2 0-22.9-6.2-25.8-11.7l-2.5-4.7h121.1c4.1 0 7.5-3.4 7.5-7.5s-3.4-7.5-7.5-7.5h-133.6c-2.6 0-5.1 1.4-6.4 3.6-1.4 2.3-1.4 5.1-0.2 7.4l8.4 15.7c5.9 11 23 19.7 39 19.7h32.1c-2.9 4.7-4.5 10.3-4.5 16.2 0 17.2 14 31.2 31.2 31.2s31.2-14 31.2-31.2c0-5.9-1.7-11.5-4.5-16.2h42.2c12.9 20.6 34.6 34.2 59.1 34.2s46.2-13.6 59.1-34.2h42.2c-2.9 4.7-4.5 10.3-4.5 16.2 0 17.2 14 31.2 31.2 31.2s31.2-14 31.2-31.2c0-5.9-1.7-11.5-4.5-16.2h32.1c16 0 33.2-8.6 39-19.7l8.4-15.7c1.2-2.3 1.2-5.1-0.2-7.4zm-345.7 58.9c0 8.9-7.3 16.2-16.2 16.2s-16.2-7.3-16.2-16.2 7.3-16.2 16.2-16.2 16.2 7.3 16.2 16.2zm104.2-210.2c0-5 2.8-9.7 7.5-12.8 4.7 3.1 7.5 7.8 7.5 12.8v72c-2.5-0.3-5-0.4-7.5-0.4s-5 0.2-7.5 0.4v-72zm7.5 213.1c-31.2 0-56.5-28.4-56.5-63.3s25.4-63.3 56.5-63.3 56.5 28.4 56.5 63.3-25.4 63.3-56.5 63.3zm144.2-3c0 8.9-7.3 16.2-16.2 16.2s-16.2-7.3-16.2-16.2 7.3-16.2 16.2-16.2 16.2 7.3 16.2 16.2z" /><path d="m264.2 250.2h-58.4c-4.1 0-7.5 3.4-7.5 7.5 0 4.1 3.4 7.5 7.5 7.5h58.4c4.1 0 7.5-3.4 7.5-7.5 0-4.1-3.4-7.5-7.5-7.5z" /></svg>
-                      <p className="airline-name">{flight.itineraries[0].segments[0].carrierCode + " " + flight.itineraries[0].segments[0].aircraft.code}</p>
-                    </div>
-
-                    <div className="flight-details">
-                      <div className="flight-rating">{flight.itineraries[0].segments[0].numberOfStops}</div>
-                      <div className="flight-timings">
-                        <span className="flight-timing-dep">{flight.itineraries[0].segments[0].departure.at}</span>
-                        <span className="line"></span>
-                        <span className="flight-timing-arr">{flight.itineraries[0].segments[0].arrival.at}</span>
-                      </div>
-                      <div className="flight-duration">{flight.duration}</div>
-                    </div>
+        <div className="flight-page-flights"> {
+          currentFlights.map((flight) => {
+            return (
+              <div className="flight-card" key={flight.id}>
+                <div className="flight-card-details">
+                  <div className="airline-details">
+                    <svg className="airline-img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 470 470"><path d="m468.9 280c-1.4-2.3-3.8-3.6-6.4-3.6h-133.5c-4.1 0-7.5 3.4-7.5 7.5s3.4 7.5 7.5 7.5h121l-2.5 4.7c-3 5.5-14.6 11.7-25.8 11.7h-120.3c3.3-9 5.2-18.8 5.2-29.1 0-14.4-3.6-28-9.9-39.6h68.4c4.1 0 7.5-3.4 7.5-7.5s-3.4-7.5-7.5-7.5h-78.9c-7.9-8.9-17.7-15.7-28.7-19.7v-75.6c0-12-7.4-22.8-19.4-28.2-1.9-0.9-4.2-0.9-6.1 0-12 5.4-19.4 16.2-19.4 28.2v75.6c-11 4-20.8 10.8-28.7 19.7h-78.9c-4.1 0-7.5 3.4-7.5 7.5s3.4 7.5 7.5 7.5h68.4c-6.3 11.6-9.9 25.2-9.9 39.6 0 10.3 1.8 20.1 5.2 29.1h-120.3c-11.2 0-22.9-6.2-25.8-11.7l-2.5-4.7h121.1c4.1 0 7.5-3.4 7.5-7.5s-3.4-7.5-7.5-7.5h-133.6c-2.6 0-5.1 1.4-6.4 3.6-1.4 2.3-1.4 5.1-0.2 7.4l8.4 15.7c5.9 11 23 19.7 39 19.7h32.1c-2.9 4.7-4.5 10.3-4.5 16.2 0 17.2 14 31.2 31.2 31.2s31.2-14 31.2-31.2c0-5.9-1.7-11.5-4.5-16.2h42.2c12.9 20.6 34.6 34.2 59.1 34.2s46.2-13.6 59.1-34.2h42.2c-2.9 4.7-4.5 10.3-4.5 16.2 0 17.2 14 31.2 31.2 31.2s31.2-14 31.2-31.2c0-5.9-1.7-11.5-4.5-16.2h32.1c16 0 33.2-8.6 39-19.7l8.4-15.7c1.2-2.3 1.2-5.1-0.2-7.4zm-345.7 58.9c0 8.9-7.3 16.2-16.2 16.2s-16.2-7.3-16.2-16.2 7.3-16.2 16.2-16.2 16.2 7.3 16.2 16.2zm104.2-210.2c0-5 2.8-9.7 7.5-12.8 4.7 3.1 7.5 7.8 7.5 12.8v72c-2.5-0.3-5-0.4-7.5-0.4s-5 0.2-7.5 0.4v-72zm7.5 213.1c-31.2 0-56.5-28.4-56.5-63.3s25.4-63.3 56.5-63.3 56.5 28.4 56.5 63.3-25.4 63.3-56.5 63.3zm144.2-3c0 8.9-7.3 16.2-16.2 16.2s-16.2-7.3-16.2-16.2 7.3-16.2 16.2-16.2 16.2 7.3 16.2 16.2z" /><path d="m264.2 250.2h-58.4c-4.1 0-7.5 3.4-7.5 7.5 0 4.1 3.4 7.5 7.5 7.5h58.4c4.1 0 7.5-3.4 7.5-7.5 0-4.1-3.4-7.5-7.5-7.5z" /></svg>
+                    <p className="airline-name">{flight.itineraries[0].segments[0].carrierCode + " " + flight.itineraries[0].segments[0].aircraft.code}</p>
                   </div>
 
-                  <div className="flight-card-price">
-                    <div className="flight-rating">{flight.price.total}</div>
+                  <div className="flight-details">
+                    <div className="flight-rating">{flight.itineraries[0].segments[0].numberOfStops}</div>
+                    <div className="flight-timings">
+                      <span className="flight-timing-dep">{flight.itineraries[0].segments[0].departure.at}</span>
+                      <span className="line"></span>
+                      <span className="flight-timing-arr">{flight.itineraries[0].segments[0].arrival.at}</span>
+                    </div>
+                    <div className="flight-duration">{flight.duration}</div>
                   </div>
                 </div>
-              )
-            })
-          }
+
+                <div className="flight-card-price">
+                  <div className="flight-rating">{flight.price.total}</div>
+                </div>
+              </div>
+            )
+          })
+        }
           <Pagination
             postsPerPage={postsPerPage}
             totalPosts={flights.length}
-            paginate={paginate}
-      />
+            paginate={paginate} />
         </div>
       </div>
     </div>
-    
   )
 }
 
--- src/components/hotel-page.component/hotel-page.component.js
+++ src/components/hotel-page.component/hotel-page.component.js
@@ -2,11 +2,61 @@ import React from "react";
 
 import "./hotel-page.component.css";
 
+const data = [
+  {
+    "name": "Hyatt Regency",
+    "city": "Ahmedabad",
+    "price": 5159,
+    "review": 8.7,
+    "address": "Plot 216, Town Plan Scheme 1, Near Vastrapur Lake, Vastrapur , Vastrapur, Ahmedabad",
+  },
+  {
+    "name": "Hyatt Regency",
+    "city": "Ahmedabad",
+    "price": 5159,
+    "review": 8.7,
+    "address": "Plot 216, Town Plan Scheme 1, Near Vastrapur Lake, Vastrapur , Vastrapur, Ahmedabad",
+  },
+  {
+    "name": "Hyatt Regency",
+    "city": "Ahmedabad",
+    "price": 5159,
+    "review": 8.7,
+    "address": "Plot 216, Town Plan Scheme 1, Near Vastrapur Lake, Vastrapur , Vastrapur, Ahmedabad",
+  },
+  {
+    "name": "Hyatt Regency",
+    "city": "Ahmedabad",
+    "price": 5159,
+    "review": 8.7,
+    "address": "Plot 216, Town Plan Scheme 1, Near Vastrapur Lake, Vastrapur , Vastrapur, Ahmedabad",
+  }
+]
 
 const HotelPage = () => {
   return (
     <div className="hotel-page-container">
-      Hotel Page
+      <div className="hotel-page-info">
+        <input className="hotel-page-search" placeholder="enter city"></input>
+        <div>check-in</div>
+        <div>check-out</div>
+        <div>Guests</div>
+        <button>Search</button>
+      </div>
+      <div className="hotel-list">
+        {
+          data.map((hotel) => {
+            return (
+              <div className="hotel-card">
+                <svg className="hotel-img" fill="white" width="50px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M501.8 148.3H388.7V30.2c0-5.6-4.6-10.2-10.2-10.2H133.5c-5.6 0-10.2 4.6-10.2 10.2v118.2H10.2C4.6 148.3 0 152.9 0 158.5v323.3c0 5.6 4.6 10.2 10.2 10.2h491.6c5.6 0 10.2-4.6 10.2-10.2v-323.3C512 152.9 507.4 148.3 501.8 148.3zM123.3 471.6H20.4V168.7h102.9V471.6zM293.7 471.6h-27.5v-27.8c0-5.6-4.6-10.2-10.2-10.2 -5.6 0-10.2 4.6-10.2 10.2v27.8h-27.5v-76.8h75.4V471.6zM368.3 471.6h-54.3v0 -87c0-5.6-4.6-10.2-10.2-10.2h-95.8c-5.6 0-10.2 4.6-10.2 10.2v87h-54.3V40.4H368.3V471.6zM491.6 471.6H388.7V168.7h102.9V471.6z" /><path d="M233 163.2h-58.2c-5.6 0-10.2 4.6-10.2 10.2v58.2c0 5.6 4.6 10.2 10.2 10.2h58.2c5.6 0 10.2-4.6 10.2-10.2v-58.2C243.2 167.7 238.6 163.2 233 163.2zM222.8 221.4h-37.8v-37.8h37.8V221.4z" /><path d="M337.2 163.2h-58.2c-5.6 0-10.2 4.6-10.2 10.2v58.2c0 5.6 4.6 10.2 10.2 10.2h58.2c5.6 0 10.2-4.6 10.2-10.2v-58.2C347.4 167.7 342.9 163.2 337.2 163.2zM327 221.4h-37.8v-37.8h37.8V221.4z" /><path d="M468 183h-56.2c-5.6 0-10.2 4.6-10.2 10.2v56.2c0 5.6 4.6 10.2 10.2 10.2h56.2c5.6 0 10.2-4.6 10.2-10.2v-56.2C478.2 187.6 473.6 183 468 183zM457.8 239.2h-35.8v-35.8h35.8V239.2z" /><path d="M468 282.4h-56.2c-5.6 0-10.2 4.6-10.2 10.2v56.2c0 5.6 4.6 10.2 10.2 10.2h56.2c5.6 0 10.2-4.6 10.2-10.2v-56.2C478.2 287 473.6 282.4 468 282.4zM457.8 338.6h-35.8V302.8h35.8V338.6z" /><path d="M468 382h-56.2c-5.6 0-10.2 4.6-10.2 10.2v56.2c0 5.6 4.6 10.2 10.2 10.2h56.2c5.6 0 10.2-4.6 10.2-10.2v-56.2C478.2 386.5 473.6 382 468 382zM457.8 438.1h-35.8V402.4h35.8V438.1z" /><path d="M99.8 183H43.7c-5.6 0-10.2 4.6-10.2 10.2v56.2c0 5.6 4.6 10.2 10.2 10.2h56.2c5.6 0 10.2-4.6 10.2-10.2v-56.2C110 187.6 105.4 183 99.8 183zM89.6 239.2H53.9v-35.8h35.8V239.2z" /><path d="M99.8 282.4H43.7c-5.6 0-10.2 4.6-10.2 10.2v56.2c0 5.6 4.6 10.2 10.2 10.2h56.2c5.6 0 10.2-4.6 10.2-10.2v-56.2C110 287 105.4 282.4 99.8 282.4zM89.6 338.6H53.9V302.8h35.8V338.6z" /><path d="M99.8 382H43.7c-5.6 0-10.2 4.6-10.2 10.2v56.2c0 5.6 4.6 10.2 10.2 10.2h56.2c5.6 0 10.2-4.6 10.2-10.2v-56.2C110 386.5 105.4 382 99.8 382zM89.6 438.1H53.9V402.4h35.8V438.1z" /><path d="M233 266.5h-58.2c-5.6 0-10.2 4.6-10.2 10.2v58.2c0 5.6 4.6 10.2 10.2 10.2h58.2c5.6 0 10.2-4.6 10.2-10.2v-58.2C243.2 271 238.6 266.5 233 266.5zM222.8 324.7h-37.8v-37.8h37.8V324.7z" /><path d="M337.2 266.5h-58.2c-5.6 0-10.2 4.6-10.2 10.2v58.2c0 5.6 4.6 10.2 10.2 10.2h58.2c5.6 0 10.2-4.6 10.2-10.2v-58.2C347.4 271 342.9 266.5 337.2 266.5zM327 324.7h-37.8v-37.8h37.8V324.7z" /><path d="M256 404.5c-5.6 0-10.2 4.6-10.2 10.2v0.9c0 5.6 4.6 10.2 10.2 10.2 5.6 0 10.2-4.6 10.2-10.2v-0.9C266.2 409.1 261.6 404.5 256 404.5z" /><path d="M275.4 65.3c-4 0-8.1 1.4-8.1 4.9v25.3h-20.5V70.1c0-3.4-4-4.9-8.1-4.9 -4 0-8.1 1.4-8.1 4.9v65.7c0 3.3 4 5 8.1 5 4 0 8.1-1.7 8.1-5V107.8h20.5v28c0 3.3 4 5 8.1 5 4 0 8.1-1.7 8.1-5V70.1C283.4 66.7 279.4 65.3 275.4 65.3z" /></svg>
+                <div className="hotel-name">{hotel.name}</div>
+                <div className="hotel-address">{hotel.address}</div>
+                <div className="hotel-rating">Review: {hotel.review}</div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
