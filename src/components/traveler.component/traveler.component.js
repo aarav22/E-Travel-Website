2e86bba103c04cbde430be9629df8c5c17ab1375
Argha Chakrabarty <ac380012@gmail.com>
2020-12-28 20:17:00 +0530

    traveler

--- src/components/traveler.component/traveler.component.js
+++ src/components/traveler.component/traveler.component.js
@@ -1,10 +1,23 @@
 import React from "react";
-
+import "./traveler.component.css"
 
 const Traveller = () => {
   return (
     <div className="traveler-container">
-      Ay Mate
+      <p className="trveler-title">Traveller Details</p>
+      <div className="traveler-adult">
+        <h6>ADULT</h6>
+        <div className="traveler-adult-forms"></div>
+      </div>
+      <div className="traveler-child">
+        <h6>CHILD</h6>
+        <div className="traveler-child-forms"></div>
+      </div>
+      <div className="contact-info">
+        <h6>CONTACT DETAILS</h6>
+        <div className="traveler-contact-forms"></div>
+      </div>
+      <button className="traveler-continue">CONTINUE</button>
     </div>
   )
 }
